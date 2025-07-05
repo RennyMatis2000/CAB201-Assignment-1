ce546ce60498132be211593f244b50ee1629f3f9
RennyMatis2000 <rennysaurus@gmail.com>
2025-07-05 20:37:20 +1000

    Add files via upload

---
+++ CAB201 Assessment Task 1 Q2 Text Censor/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+namespace Q2
+{
+    public class Q2
+    {
+        static void Main(string[] args)
+        {
+            // Insert your solution here.
+            Console.WriteLine("Enter the text to be censored:");
+
+            string enteredText = Console.ReadLine();
+
+            Console.WriteLine("Enter the number of phrases to censor:");
+
+            string numberPhrasesString = Console.ReadLine();
+            int numberPhrases = Convert.ToInt32(numberPhrasesString);
+            string censoredText = enteredText;
+
+            for (int i = 0; i < numberPhrases; i++)
+            {
+                Console.WriteLine("Phrase:");
+                string phrases = Console.ReadLine();
+                int phraseLength = phrases.Length;
+                string censor = new string('*', phraseLength);
+                censoredText = censoredText.Replace(phrases, censor);
+            }
+            Console.WriteLine($"The censored text is as follows: {censoredText}");
+        }
+    }
+}
