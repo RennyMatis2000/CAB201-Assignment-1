6919b0549c39a939e8020fd6a90d2057b8de5e86
RennyMatis2000 <rennysaurus@gmail.com>
2025-07-05 20:39:31 +1000

    Add files via upload

---
+++ CAB201 Assessment Task 1 Q4 Duplicate Counter/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace Q4
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please provide a list of integers in a single line, separated by _:");
+            string integerList = Console.ReadLine();
+            integerList = integerList.Trim();
+            string[] splitIntegerList = integerList.Split("_");
+            int duplicateCounter = 0;
+
+            List<int> duplicates = new List<int>();
+
+            for (int i = 0; i < splitIntegerList.Length; i++) 
+            {
+
+                if (!string.IsNullOrWhiteSpace(splitIntegerList[i]))
+                {
+                    string trimmedIntegerList = splitIntegerList[i].Trim();
+
+                    if(int.TryParse(trimmedIntegerList, out int guaranteedInteger))
+                    {
+                        duplicates.Add(guaranteedInteger);
+                    }
+                }
+            }
+
+            duplicates.Sort();
+
+            for (int i = 0; i < duplicates.Count - 1; i++)
+            {
+                if (duplicates[i] == duplicates[i + 1])
+                {
+                    duplicateCounter++;
+
+                    while (i < duplicates.Count - 1 && duplicates[i] == duplicates[i + 1])
+                    {
+                        i++;
+                    }
+                }
+            }
+
+            Console.WriteLine($"The total number of values with repeats is {duplicateCounter}.");
+
+           
+            
+            
+
+
+        }
+    }
+}
